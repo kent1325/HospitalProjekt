d92f1747c1cea6b6be33794b2b30289c4f8cbe84
kent nielsen <kent.vugs@hotmail.com>
2018-02-20 10:43:00 +0100

    CREATE, READ og DELETE funktionalitet for patient er oprettet

--- Hospital.PatientMeasurement/MainWindow.xaml.cs
+++ Hospital.PatientMeasurement/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Hospital.PatientMeasurement.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,68 @@ namespace Hospital.PatientMeasurement
         public MainWindow()
         {
             InitializeComponent();
+
+            List<Patients> patients;
+
+            using (HospitalDatabase db = new HospitalDatabase())
+            {
+                patients = db.Patients.ToList();
+            }
+
+
+            patient_dataGrid.ItemsSource = patients;
+        }
+
+        private void patientAdd_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            using (HospitalDatabase db = new HospitalDatabase())
+            {
+                Patients patient = new Patients();
+                patient.PatientID = Guid.NewGuid();
+                patient.PatientName = "Test Person";
+                patient.PatientEmail = "test@test.com";
+                patient.PatientCPR = "012345-678";
+                patient.PatientAddress = "Adresse 1";
+                patient.PatientPostalCode = 1234;
+
+                db.Patients.Add(patient);
+                db.SaveChanges();
+                UpdateDatagrid();
+            }
+        }
+
+        private void patientDelete_menuItem_Click(object sender, RoutedEventArgs e)
+        {
+            using (HospitalDatabase db = new HospitalDatabase())
+            {
+                try
+                {
+                    Patients test = patient_dataGrid.SelectedItem as Patients;
+                    List<Patients> patient;
+                    patient = db.Patients.Where(p => p.PatientID == test.PatientID).ToList();
+                    db.Patients.Remove(patient.FirstOrDefault());
+                    db.SaveChanges();
+                    UpdateDatagrid();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void UpdateDatagrid()
+        {
+            patient_dataGrid.ItemsSource = null;
+            using (HospitalDatabase db = new HospitalDatabase())
+            {
+                patient_dataGrid.ItemsSource = db.Patients.ToList();
+            }
+        }
+
+        private void patientEdit_menuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Du vil redigere noget!");
         }
     }
 }
